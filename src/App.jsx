cb1315d289ee92041684b35deabf3fbb033de6fd
bhavani <bhavani@gmail.com>
2025-05-27 15:05:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar.jsx";
+import Houses from "./Houses.jsx";
+import Aboutname from "./Aboutname.jsx";
+import Data from "./Data.jsx";
+import Footer from"./Footer.jsx";
+function App(){
+  return(
+    <div>
+         <Navbar />
+         <Houses />
+         <Aboutname/>
+         <Data />
+         <Footer />
+    </div>
+  )
+}
+export default App;
